ca74b104c5c5e9f4c9f6b9d9abad27712c818496
ofnullable <ofnullable@gmail.com>
2020-04-12 20:35:36 +0900

    :sparkles: Add SSR codes in src/server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,65 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import { resolve } from 'path';
+import fs from 'fs';
 
-const html = renderToString(
-  <div>Hi server!</div>,
+import App from './App';
+
+const app = express();
+
+const manifest = JSON.parse(
+  fs.readFileSync(resolve('./build/asset-manifest.json'), 'utf8'),
 );
 
-console.log(html);
+const chunks = Object.keys(manifest.files)
+  .filter(name => /chunk\.js$/.exec(name))
+  .map(name => `<script src="${manifest.files[name]}"></script>`)
+  .join('');
 
+const createHtml = root => `
+  <!DOCTYPE html>
+  <html lang="ko">
+  <head>
+      <meta charset="utf-8"/>
+      <link rel="shortcut icon" href="/favicon.ico"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+      <meta name="theme-color" content="#000000"/>
+      <meta name="description" content="Web site created using create-react-app"/>
+      <link rel="apple-touch-icon" href="/logo192.png"/>
+      <link rel="stylesheet" href="${manifest.files['main.css']}"/>
+      <title>React App</title>
+  </head>
+  <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root">${root}</div>
+      <script src="${manifest.files['runtime-main.js']}"></script>
+      ${chunks}
+      <script src="${manifest.files['main.js']}"></script>
+  </body>
+  </html>
+`;
+
+app.use(express.static(resolve('./build'), {
+  index: false,
+}));
+
+app.use(async (req, res, next) => {
+  const context = {};
+
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App/>
+    </StaticRouter>
+  );
+
+  res.set('content-type', 'text/html');
+  const root = renderToString(jsx);
+  res.send(createHtml(root));
+});
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+  console.log(`React + Express server running on port ${port}`);
+});
